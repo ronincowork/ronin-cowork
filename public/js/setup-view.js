778d072c5a4ca55d3d985ea823be71aa992220ff
Ronin session setup_live_audit <setup_live_audit@sessions.ronin.local>
2026-09-20 09:42:47 +0000

    Simplify Setup workbench header

--- public/js/setup-view.js
+++ public/js/setup-view.js
@@ -276,7 +276,7 @@ export function createSetupView() {
   return {
     el: bench.host,
     glyph: '人',
-    ...workbenchView('setup', { header: { actions: [setupStepsHeader, themeToggle] } }),
+    ...workbenchView('setup', { header: { ram: false, feedback: false, actions: [setupStepsHeader, themeToggle] } }),
     title: () => 'Ronin Setup',
     mount: (_host, context) => { ctx = context; },
     enter: async (context) => {
